e897245940254dd661f4568056453b6af80f335c
吴占超 <zhanchao.wu@icloud.com>
2018-10-17 09:15:16 +0800

    sql

--- restfulapi/src/main/java/wzc/buyer/RestfulapiApplication.java
+++ restfulapi/src/main/java/wzc/buyer/RestfulapiApplication.java
@@ -1,7 +1,9 @@
 package wzc.buyer;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 /**
  * 启动类
@@ -9,6 +11,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @date 2018/10/16 5:17 PM
  */
 @SpringBootApplication
+@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
 public class RestfulapiApplication {
 
     public static void main(String[] args) {
